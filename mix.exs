4801b1e6c18b5aa3bdd0aa4192f8c27ec5db00f0
Paul Friedman <paulrfri@gmail.com>
2017-02-21 10:50:14 -0800

    Add credo and fix minor issues

--- mix.exs
+++ mix.exs
@@ -35,7 +35,8 @@ defmodule DoubleRed.Mixfile do
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:credo, "~> 0.5", only: [:dev, :test]}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
--- web/models/waft.ex
+++ web/models/waft.ex
@@ -1,4 +1,11 @@
 defmodule DoubleRed.Waft do
+  @moduledoc """
+  A Waft is a single reading from the DoubleRed hardware sensor.
+
+  The sensor provides red, green and blue color values, as well as color
+  temperature and brightness (lumen) readings.
+  """
+
   use DoubleRed.Web, :model
 
   schema "wafts" do
@@ -20,23 +27,32 @@ defmodule DoubleRed.Waft do
     |> validate_required([:temperature, :lumens, :red, :green, :blue])
     |> check_constraint(:temperature, name: :positive_temperature,
       message: "temperature must be positive"
-    )|> check_constraint(:temperature, name: :valid_temperature,
+    )
+    |> check_constraint(:temperature, name: :valid_temperature,
       message: "temperature must be below 65536"
-    )|> check_constraint(:lumens, name: :positive_lumens,
+    )
+    |> check_constraint(:lumens, name: :positive_lumens,
       message: "lumens must be positive"
-    )|> check_constraint(:lumens, name: :valid_lumens,
+    )
+    |> check_constraint(:lumens, name: :valid_lumens,
       message: "lumens must be below 65536"
-    )|> check_constraint(:red, name: :positive_red,
+    )
+    |> check_constraint(:red, name: :positive_red,
       message: "red must be positive"
-    )|> check_constraint(:red, name: :valid_red,
+    )
+    |> check_constraint(:red, name: :valid_red,
       message: "red must be below 65536"
-    )|> check_constraint(:green, name: :positive_green,
+    )
+    |> check_constraint(:green, name: :positive_green,
       message: "green must be positive"
-    )|> check_constraint(:green, name: :valid_green,
+    )
+    |> check_constraint(:green, name: :valid_green,
       message: "green must be below 65536"
-    )|> check_constraint(:blue, name: :positive_blue,
+    )
+    |> check_constraint(:blue, name: :positive_blue,
       message: "blue must be positive"
-    )|> check_constraint(:blue, name: :valid_blue,
+    )
+    |> check_constraint(:blue, name: :valid_blue,
       message: "blue must be below 65536"
     )
   end
