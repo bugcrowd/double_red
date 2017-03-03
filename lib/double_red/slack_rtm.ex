f5c8b8a67e84d3a51a0c40d0b2531de36d530ffc
dradford <damienradford@gmail.com>
2017-03-03 12:52:44 -0800

    test stuff

--- lib/double_red/slack_rtm.ex
+++ lib/double_red/slack_rtm.ex
@@ -9,15 +9,15 @@ defmodule DoubleRed.SlackRtm do
   alias Slack.Lookups
 
   @welcome_message ~s"""
-    Hi there! Since this is our first conversation, let me tell you a bit about how I work.
+Hi there! Since this is our first conversation, let me tell you a bit about how I work.
 
-    Watch my status to quickly see if there's a free bathroom. Green means go! Other than that, anytime you message me I'll let you know which downstairs bathroom, if any, is free.
+Watch my status to quickly see if there's a free bathroom. Green means go! Other than that, anytime you message me I'll let you know which downstairs bathroom, if any, is free.
 
-    I understand you work for a security company, so you're probably looking for some technical details. I work using a color sensor (https://www.adafruit.com/products/1334) attached to a WiFi microcontroller (https://www.adafruit.com/product/2471). Every second or so I send the color I read from the lock label to a server (https://double-red.herokuapp.com/api/wafts). The color information I pick up is transient -— I only hold on to as many records as I need to function. This information is conveniently distilled for you using Slack.
+I understand you work for a security company, so you're probably looking for some technical details. I work using a color sensor (https://www.adafruit.com/products/1334) attached to a WiFi microcontroller (https://www.adafruit.com/product/2471). Every second or so I send the color I read from the lock label to a server (https://double-red.herokuapp.com/api/wafts). This information is conveniently distilled for you using Slack.
 
-    Cheers,
-    Double Red
-  """
+Cheers,
+Double Red
+"""
 
   def handle_connect(slack, state) do
     IO.puts "Connected as #{slack.me.name}"
